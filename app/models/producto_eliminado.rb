84de739b20f5ab9980dfe77b8741798b203166ee
Lahiri Sanchez <lahiri_sanchez@hotmail.com>
2014-04-10 17:58:04 -0430

    Modulo Priducto modelos producto_eliminado

--- app/models/producto_eliminado.rb
+++ app/models/producto_eliminado.rb
@@ -1,5 +1,8 @@
 class ProductoEliminado < ActiveRecord::Base
   attr_accessible :codigo_prod, :descripcion, :fecha_creacion, :gpc, :gtin, :id_estatus, :id_tipo_gtin, :marca
   self.table_name = "producto_eliminado"
+  belongs_to :productos_empresa, :primary_key => "gtin",  :foreign_key => "gtin" # busca los productos en productos_empresa a traves de gtin no del campo  id
   has_one :producto_elim_detalle, :foreign_key => "gtin", :dependent => :destroy # eliminacion en cascada
+  belongs_to :estatus, :foreign_key => "id_estatus"
+  belongs_to :tipo_gtin, :foreign_key => "id_tipo_gtin"
 end
