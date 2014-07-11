623b734059e09ab9defbbaca6780f64201665bfc
Lahiri Sanchez <lahiri_sanchez@hotmail.com>
2014-07-11 11:47:51 -0430

    Cambios Requerimientos GS1 - 4

--- app/datatables/retirar_productos_datatable.rb
+++ app/datatables/retirar_productos_datatable.rb
@@ -30,7 +30,6 @@ private
         producto.gtin,
         producto.descripcion,
         producto.marca,
-        producto.gpc,
         producto.try(:estatus).try(:descripcion),
         producto.codigo_prod,
         fecha,
@@ -50,7 +49,7 @@ private
   end
 
   def fetch_productos
-     productos = Producto.where("productos_empresa.prefijo = ?", params[:empresa_id]).includes({:productos_empresa => :empresa}, :estatus, :tipo_gtin).order("producto.fecha_creacion") 
+     productos = Producto.where("productos_empresa.prefijo = ? and estatus.descripcion = ?", params[:empresa_id], 'Activo').includes({:productos_empresa => :empresa}, :estatus, :tipo_gtin).order("producto.fecha_creacion") 
     #productos = Producto.where("productos_empresa.prefijo = ? and estatus.descripcion like ? and estatus.alcance like ?",params[:empresa_id], 'Activo', 'Producto').includes({:productos_empresa => :empresa}, :estatus, :tipo_gtin, {:productos_retirados => :sub_estatus}, {:productos_retirados => :motivo_retiro})
     productos = productos.page(page).per_page(per_page)
     
@@ -78,31 +77,13 @@ private
       productos = productos.where("producto.marca like :search5", search5: "%#{params[:sSearch_5]}%" )
     end
 
-    if params[:sSearch_6].present?
-      productos = productos.where("producto.gpc like :search6", search6: "%#{params[:sSearch_6]}%" )
-    end
-
     if params[:sSearch_7].present?
-      productos = productos.where("estatus.descripcion like :search7", search7: "%#{params[:sSearch_7]}%")
+      productos = productos.where("producto.codigo_prod like :search7", search7: "%#{params[:sSearch_7]}%" )
     end
 
     if params[:sSearch_8].present?
-      productos = productos.where("producto.codigo_prod like :search8", search8: "%#{params[:sSearch_8]}%" )
+      productos = productos.where("producto.fecha_creacion like :search8", search8: "%#{params[:sSearch_8]}%" )
     end
-
-    if params[:sSearch_9].present?
-      productos = productos.where("producto.fecha_creacion like :search9", search9: "%#{params[:sSearch_9]}%" )
-    end
-    
-    # if params[:sSearch_10].present?
-    #   productos = productos.where("sub_estatus.descripcion like :search10", search10: "%#{params[:sSearch_10]}%" )
-    # end
-
-    # if params[:sSearch_11].present?
-    #   productos = productos.where("motivo_retiro.descripcion like :search11", search11: "%#{params[:sSearch_11]}%" )
-    # end
-
-
     
     productos
 
