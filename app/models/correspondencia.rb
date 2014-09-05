32d37b229d684fb85b0e603eab656b46027d4cad
Lahiri Sanchez <lsanchez@scannven.com.ve>
2014-09-04 20:52:19 -0430

    cambios locales

--- app/models/correspondencia.rb
+++ app/models/correspondencia.rb
@@ -1,7 +1,7 @@
 class Correspondencia < ActiveRecord::Base
-  attr_accessible :calle, :cargo_rep_tecnico, :cod_postal, :edificio, :id_ciudad, :id_estado, :id_municipio, :id_parroquia, :punto_referencia, :rep_tecnico, :urbanizacion, :nombre_parroquia, :tipo_correspondencia, :correspondencia, :direccion, :piso, :detalle_piso
+  attr_accessible :calle, :cargo_rep_tecnico, :cod_postal, :edificio, :id_ciudad, :id_estado, :id_municipio, :id_parroquia, :punto_referencia, :rep_tecnico, :urbanizacion, :nombre_parroquia, :tipo_correspondencia, :direccion, :piso, :detalle_piso
    self.table_name = "empresa_correspondencia"
-   belongs_to :empresa, :foreign_key => "prefijo"
+   #belongs_to :empresa, :foreign_key => "prefijo"
    belongs_to :estado, :foreign_key => "id_estado"
    belongs_to :ciudad, :foreign_key => "id_ciudad"
    belongs_to :municipio, :foreign_key => "id_municipio"
