4e8074c07af25f3fe923686f1591b93eefc8c3f7
Jonathan Acosta <jgacosta@gs1ve.org>
2014-09-24 11:50:36 -0430

    migraciones pendientes

---
+++ db/migrate/20140923174330_agregar_motivo_retiro_id_motivo_retiro_empresa.rb
@@ -0,0 +1,4 @@
+class AgregarMotivoRetiroIdMotivoRetiroEmpresa < ActiveRecord::Migration
+  add_column :empresa, :id_motivo_retiro, :integer
+  add_column :empresa, :fecha_retiro, :datetime
+end
---
+++ db/migrate/20140923182757_agregar_mantenimiento_empresa.rb
@@ -0,0 +1,4 @@
+class AgregarMantenimientoEmpresa < ActiveRecord::Migration
+  add_column :empresa, :aporte_mantenimiento_ut, :float
+  add_column :empresa, :aporte_mantenimiento_bs, :float
+end
