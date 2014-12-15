639f9b72f260fc5a11676a0560ffa5b113271824
Lahiri Sanchez <lsanchez@scannven.com.ve>
2014-12-15 14:33:11 -0430

    migracion

--- db/migrate/20141211153828_empresa_registrada_datos_administrativos.rb
+++ db/migrate/20141211153828_empresa_registrada_datos_administrativos.rb
@@ -9,7 +9,7 @@ class EmpresaRegistradaDatosAdministrativos < ActiveRecord::Migration
 	add_column :empresas_registradas, :fax_ean_completo, :string
 
 	add_column :empresa, :contacto_tlf1_completo, :string
-	add_column :empresa, :contacto_tlf2_completo, :string
+	#add_column :empresa, :contacto_tlf2_completo, :string
 	add_column :empresa, :contacto_tlf3_completo, :string
 	add_column :empresa, :contacto_fax_completo, :string
 	add_column :empresa, :telefono1_ean_completo, :string
