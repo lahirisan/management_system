4d90e87980a87be4bc1da7daa33948ce95c3092e
alpha_developer <lahiri.sanchez@gmail.com>
2015-01-26 11:39:53 -0430

    se comenta fecha de activacion

--- app/datatables/empresas_datatable.rb
+++ app/datatables/empresas_datatable.rb
@@ -31,7 +31,7 @@ private
           [ 
             empresa.prefijo,
             empresa.nombre_empresa,
-            fecha,
+            "",
             empresa.ciudad_,
             empresa.rif_completo,
             empresa.estatus_.upcase,
@@ -50,7 +50,7 @@ private
           [
             empresa.prefijo,
             empresa.nombre_empresa,
-            fecha,
+            "",
             empresa.ciudad_,
             empresa.rif_completo,
             empresa.estatus_.upcase,
@@ -67,7 +67,7 @@ private
           [
             empresa.prefijo,
             empresa.nombre_empresa,
-            fecha,
+            "",
             empresa.ciudad_,
             empresa.rif_completo,
             empresa.estatus_.upcase,
@@ -93,7 +93,7 @@ private
           [ 
           empresa.prefijo,
           empresa.nombre_empresa,
-          fecha,
+          "",
           empresa.ciudad_,
           empresa.rif_completo,
           empresa.estatus_.upcase,
@@ -108,7 +108,7 @@ private
           [ 
           empresa.prefijo,
           empresa.nombre_empresa,
-          fecha,
+          "",
           empresa.ciudad_,
           empresa.rif_completo,
           empresa.estatus_.upcase,
