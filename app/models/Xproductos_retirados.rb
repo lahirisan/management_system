467269c4b9ab4868ecee727eebdee5bbdb0f04b2
alpha_developer <lahiri.sanchez@gmail.com>
2015-02-10 17:35:44 -0430

    eliminando archivos innecesarios

--- app/models/Xproductos_retirados.rb
+++
@@ -1,7 +0,0 @@
-class ProductosRetirados < ActiveRecord::Base
-  attr_accessible :fecha_retiro, :gtin, :id_usuario, :id_motivo_retiro, :id_subestatus
-  belongs_to :producto, :foreign_key => "gtin"
-  belongs_to :sub_estatus, :foreign_key => "id_subestatus"
-  belongs_to :motivo_retiro, :foreign_key => "id_motivo_retiro"
-
-end
