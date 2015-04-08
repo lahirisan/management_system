38cb5e374bb7368eaa5c732d3c7d659e91088298
alpha_developer <lahiri.sanchez@gmail.com>
2015-04-08 16:13:01 -0430

    Activar empresa

--- app/controllers/empresa_registradas_controller.rb
+++ app/controllers/empresa_registradas_controller.rb
@@ -33,7 +33,7 @@ class EmpresaRegistradasController < ApplicationController
 
           render json: (EmpresaRegistradasActivarSolvenciaDatatable.new(view_context)) 
         else
-
+          
           render json: (EmpresaRegistradasDatatable.new(view_context)) 
         end
 
@@ -73,14 +73,25 @@ class EmpresaRegistradasController < ApplicationController
       format.html{
         
         if params[:activar_empresa]
-
+          
           @prefijo = Empresa.generar_prefijo_valido
           @clasificacion_empresa = Clasificacion.find(:first, :conditions => ["categoria = ? and division = ? and grupo = ? and clase = ?", @empresa_registrada.categoria, @empresa_registrada.division, @empresa_registrada.grupo, @empresa_registrada.clase])
           @prefijos_disponibles = EmpresaEliminada.find(:all, :include => [:estatus], :conditions => ["(categoria = ? or division = ? or grupo = ? or clase = ?) and no_elejible is ? and prefijo >= 7590000 and prefijo <= 7599999", @empresa_registrada.categoria, @empresa_registrada.division, @empresa_registrada.grupo, @empresa_registrada.clase, nil], :select => "empresa.prefijo, empresa.nombre_empresa, clasificacion.descripcion, estatus.descripcion")
 
+          # Se busca el prefijo encontrado contra el sistema adminsitrativo para asegurarse que este disponible
+          @prefijo_asignado_administrativo = Empresa.find_by_sql " Select * from [BDGS1DTS.MDF].dbo.CC_Clientes where codigo = #{@prefijo}"
+
+          flash[:warning] = "La Aplicación detectó que el prefijo disponible en Sistema de Gestión #{@prefijo} se encuentra asignado en la Base de Datos del Sistema Administrativo. De continuar se producirá un error. Se sugiere que asigne el prefijo manual, y verifique que no se encuentre ya asignado en Sistema Administrativo." if !(@prefijo_asignado_administrativo.empty?)
+
+
         end
 
-        render :template => '/empresa_registradas/_form.html.haml'
+        if params[:activar_empresa]
+
+          render :template => '/empresa_registradas/_form_activar.html.haml'
+        else
+          render :template => '/empresa_registradas/_form.html.haml'
+        end
         
       }
 
@@ -91,13 +102,48 @@ class EmpresaRegistradasController < ApplicationController
   # POST /empresa_registradas
   # POST /empresa_registradas.json
   def create
+
+
     
     params[:empresa_registrada][:id_estatus] = 5 # NO VALIDADA (SIN PREFIJO)
     params[:empresa_registrada][:id_subestatus] = 2 # NO SOLVENTE
     params[:empresa_registrada][:fecha_inscripcion] = Time.now
-    params[:empresa_registrada][:id_tipo_usuario] = 3 if params[:empresa_registrada][:id_tipo_usuario] == '' # Si el usaurio no especifico el tipo de empresa
+    
+    params[:empresa_registrada][:nombre_empresa] = params[:empresa_registrada][:nombre_empresa].upcase if params[:empresa_registrada][:nombre_empresa]
+    params[:empresa_registrada][:nombre_comercial] =  params[:empresa_registrada][:nombre_comercial] if params[:empresa_registrada][:nombre_comercial]
+    params[:empresa_registrada][:nombre_comercial] = params[:empresa_registrada][:nombre_empresa].upcase if params[:empresa_registrada][:nombre_comercial].blank? 
+    params[:empresa_registrada][:direccion_fiscal] = params[:empresa_registrada][:direccion_fiscal].upcase if params[:empresa_registrada][:direccion_fiscal]
+    params[:empresa_registrada][:circunscripcion_judicial] = params[:empresa_registrada][:circunscripcion_judicial].upcase if params[:empresa_registrada][:circunscripcion_judicial]
+    params[:empresa_registrada][:rep_legal] = params[:empresa_registrada][:rep_legal].upcase if params[:empresa_registrada][:rep_legal]
+    params[:empresa_registrada][:cargo_rep_legal] = params[:empresa_registrada][:cargo_rep_legal].upcase if params[:empresa_registrada][:cargo_rep_legal]
+    params[:empresa_registrada][:nacionalidad_responsable_legal] = params[:empresa_registrada][:nacionalidad_responsable_legal].upcase if params[:empresa_registrada][:nacionalidad_responsable_legal]
+    params[:empresa_registrada][:domicilio_responsable_legal] = params[:empresa_registrada][:domicilio_responsable_legal].upcase if params[:empresa_registrada][:domicilio_responsable_legal]
+    params[:empresa_registrada][:rep_ean] = params[:empresa_registrada][:rep_ean].upcase if params[:empresa_registrada][:rep_ean]
+    params[:empresa_registrada][:rep_ean_cargo] = params[:empresa_registrada][:rep_ean_cargo].upcase if params[:empresa_registrada][:rep_ean_cargo]
+    params[:empresa_registrada][:tipo_galpon_edificio_quinta] = params[:empresa_registrada][:tipo_galpon_edificio_quinta].upcase if params[:empresa_registrada][:tipo_galpon_edificio_quinta]
+    params[:empresa_registrada][:galpon_edificio_quinta] = params[:empresa_registrada][:galpon_edificio_quinta].upcase if params[:empresa_registrada][:galpon_edificio_quinta]
+    params[:empresa_registrada][:tipo_piso_numero] = params[:empresa_registrada][:tipo_piso_numero].upcase if params[:empresa_registrada][:tipo_piso_numero]
+    params[:empresa_registrada][:tipo_oficina_apartamento] = params[:empresa_registrada][:tipo_oficina_apartamento].upcase if params[:empresa_registrada][:tipo_oficina_apartamento]
+    params[:empresa_registrada][:oficina_apartamento] = params[:empresa_registrada][:oficina_apartamento].upcase if params[:empresa_registrada][:oficina_apartamento]
+    params[:empresa_registrada][:tipo_avenida_calle]  = params[:empresa_registrada][:tipo_avenida_calle].upcase if params[:empresa_registrada][:tipo_avenida_calle]
+    params[:empresa_registrada][:avenida_calle] = params[:empresa_registrada][:avenida_calle].upcase if params[:empresa_registrada][:avenida_calle]
+    params[:empresa_registrada][:tipo_urbanizacion_barrio_sector] = params[:empresa_registrada][:tipo_urbanizacion_barrio_sector].upcase if params[:empresa_registrada][:tipo_urbanizacion_barrio_sector]
+    params[:empresa_registrada][:urbanizacion_barrio_sector] = params[:empresa_registrada][:urbanizacion_barrio_sector].upcase if params[:empresa_registrada][:urbanizacion_barrio_sector]+
+    params[:empresa_registrada][:parroquia_ean] = params[:empresa_registrada][:parroquia_ean].upcase if params[:empresa_registrada][:parroquia_ean]
+    params[:empresa_registrada][:punto_ref_ean] = params[:empresa_registrada][:punto_ref_ean].upcase if params[:empresa_registrada][:punto_ref_ean]
+    params[:empresa_registrada][:rep_edi] = params[:empresa_registrada][:rep_edi].upcase if params[:empresa_registrada][:rep_edi]
+    params[:empresa_registrada][:rep_edi_cargo] = params[:empresa_registrada][:rep_edi_cargo].upcase if params[:empresa_registrada][:rep_edi_cargo]
+    params[:empresa_registrada][:tipo_galpon_edificio_quinta_sincronet] = params[:empresa_registrada][:tipo_galpon_edificio_quinta_sincronet].upcase if params[:empresa_registrada][:tipo_galpon_edificio_quinta_sincronet]
+    params[:empresa_registrada][:galpon_edificio_quinta_sincronet] = params[:empresa_registrada][:galpon_edificio_quinta_sincronet].upcase if params[:empresa_registrada][:galpon_edificio_quinta_sincronet]
+    params[:empresa_registrada][:tipo_piso_numero_sincronet] = params[:empresa_registrada][:tipo_piso_numero_sincronet].upcase  if params[:empresa_registrada][:tipo_piso_numero_sincronet]
+    params[:empresa_registrada][:piso_numero_sincronet] =  params[:empresa_registrada][:piso_numero_sincronet].upcase if params[:empresa_registrada][:piso_numero_sincronet]
 
-    params[:empresa_registrada][:nombre_comercial] = params[:empresa_registrada][:nombre_empresa] if params[:empresa_registrada][:nombre_comercial].blank? 
+
+
+
+
+
+
     params[:empresa_registrada][:cod_contacto_tlf3] = params[:empresa_registrada][:cod_contacto_tlf1] if params[:empresa_registrada][:cod_contacto_tlf3].blank?
     params[:empresa_registrada][:contacto_tlf3] = params[:empresa_registrada][:contacto_tlf1] if params[:empresa_registrada][:contacto_tlf3].blank?
     params[:empresa_registrada][:cod_contacto_fax] = params[:empresa_registrada][:cod_contacto_tlf1] if params[:empresa_registrada][:cod_contacto_fax].blank?
@@ -174,7 +220,7 @@ class EmpresaRegistradasController < ApplicationController
     params[:empresa_registrada][:telefono3_ean_completo] = "("+ params[:empresa_registrada][:cod_tlf3_ean] + ")" +" "+ params[:empresa_registrada][:telefono3_ean] if params[:empresa_registrada][:cod_tlf3_ean] != ""
     params[:empresa_registrada][:fax_ean_completo] = "("+ params[:empresa_registrada][:cod_fax_ean] + ")" +" "+ params[:empresa_registrada][:fax_ean]              if params[:empresa_registrada][:cod_fax_ean] != ""  
 
-
+    
 
     @opciones = ['J', 'G', 'E', 'V']
 
@@ -202,9 +248,10 @@ class EmpresaRegistradasController < ApplicationController
     end
   end
 
-  def update_multiple  ## Ruta para asignar SOLVENTE  a las nuevas empresas  
+  def update_multiple  ## Ruta para asignar SOLVENTE  a las nuevas empresas  metodo patch 
     
 
+  
     nuevas_empresas = EmpresaRegistrada.find(params[:activar_solvencias])
     solvente = SubEstatus.find(:first, :conditions => ["descripcion = ?", "SOLVENTE"])
 
