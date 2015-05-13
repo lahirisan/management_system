288777c7caa183f56d6a2c7885f8cc700fb6db0d
alpha_developer <lahiri.sanchez@gmail.com>
2015-05-13 17:05:28 -0430

    configuracion trabajar con nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/var/www/desarrollo/GS1"
-pid "/var/www/desarrollo/GS1/tmp/pids/unicorn.pid"
-stderr_path "/var/www/desarrollo/GS1/log/unicorn.log"
-stdout_path "/var/www/desarrollo/GS1/log/unicorn.log"
+working_directory "/home/scannven/GS1"
+pid "/home/scannven/GS1/tmp/pids/unicorn.pid"
+stderr_path "/home/scannven/GS1/log/unicorn.log"
+stdout_path "/home/scannven/GS1/log/unicorn.log"
 
 listen "/tmp/unicorn.GS1.sock"
 worker_processes 2
