7e73681ebce83d4a7e03321cd49a44b4035ba474
Jonathan Acosta <jgacosta@gs1ve.org>
2015-05-13 14:24:54 -0430

    configuracion nginx

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+working_directory "/var/www/desarrollo/GS1"
+pid "/var/www/desarrollo/GS1/tmp/pids/unicorn.pid"
+stderr_path "/var/www/desarrollo/GS1/log/unicorn.log"
+stdout_path "/var/www/desarrollo/GS1/log/unicorn.log"
+
+listen "/tmp/unicorn.GS1.sock"
+worker_processes 2
+timeout 30
+
+
