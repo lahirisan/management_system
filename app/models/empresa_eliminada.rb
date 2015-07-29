1bbb2e7ed75358c713f9eeee8cc24b1947636565
alpha_developer <lahiri.sanchez@gmail.com>
2015-07-29 10:36:43 -0430

    La seleccion de las empresas para activar.

--- app/models/empresa_eliminada.rb
+++ app/models/empresa_eliminada.rb
@@ -1,5 +1,5 @@
 class EmpresaEliminada < ActiveRecord::Base
-  attr_accessible :rep_legal, :categoria, :clase, :division, :grupo,  :id_estatus, :id_tipo_usuario, :nombre_empresa, :rif, :id_motivo_retiro
+  attr_accessible :rep_legal, :categoria, :clase, :division, :grupo,  :id_estatus, :id_tipo_usuario, :nombre_empresa, :rif, :id_motivo_retiro, :no_elegible
   self.table_name = "empresa_eliminada"  # El nombre de la tabla que se esta mapeando
   belongs_to :estatus, :foreign_key =>  "id_estatus"
   belongs_to :motivo_retiro, :foreign_key => "id_motivo_retiro"
